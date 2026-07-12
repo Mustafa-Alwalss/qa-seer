f95c12030438a67091392a79ff9ba226c3d1d173
Mustafa Alwalss <malwalss33@gmail.com>
2026-07-12 17:48:55 +0300

    initialization spring boot

---
+++ back-end/src/main/java/com/mustafa/qa_seer/QaSeerApplication.java
@@ -0,0 +1,13 @@
+package com.mustafa.qa_seer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaSeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaSeerApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/mustafa/qa_seer/QaSeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustafa.qa_seer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaSeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
